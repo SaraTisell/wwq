5bc35ad68954fa24a4a3b3ab55871c860794d424
SaraTisell <>
2023-11-13 14:03:44 +0000

    Add questions for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+    {
+        question: 'How many countries is there in the World today?',
+        options: ['205', '150', '195'],
+        answer: '195',
+    },
+    {
+        question: 'What is the national flower of Japan?',
+        options: ['Cherry blossom', 'Lily', 'Rose'],
+        answer: 'Cherry blossom',
+    },
+    {
+        question: 'Who invented the World Wide Web, and when?',
+        options: ['Thomas Eddison, 1978', 'Tim Berners-Lee, 1990 ', 'Alice Jameson, 1989'],
+        answer: 'Tim Berners-Lee, 1990',
+    },
+    {
+        question: 'What was the clothing company Nike originally called?',
+        options: ['Nikers', 'Blue Ribbon Sports', 'Swoosh'],
+        answer: 'Blue Ribbon Sports',
+    },
+    {
+        question: 'When was Netflix founded?',
+        options: ['1997, '2005', '2011'],
+        answer: '1997',
+    },
+]
